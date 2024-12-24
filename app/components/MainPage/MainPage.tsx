2dc393270d2d87aa3f3e20fe0dc6f1ba9b55773c
Arman <aaproyan@bk.ru>
2024-12-24 13:52:32 +0400

    favicone

--- app/components/MainPage/MainPage.tsx
+++ app/components/MainPage/MainPage.tsx
@@ -51,6 +51,9 @@ const MainPage = () => {
       <Head>
         <title>Ремонт обуви и изготовление ключей | Новая Походка</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <meta
           name="description"
           content="Мы предоставляем услуги по ремонту обуви и изготовлению ключей. Работает с 11:00 до 20:00 без выходных."
