d91833ffbde134effdec4ad72dfa8393981db707
Arman <aaproyan@bk.ru>
2025-01-02 23:17:36 +0400

    change styles

--- app/components/isOpen/IsOpen.tsx
+++ app/components/isOpen/IsOpen.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { useEffect, useState } from 'react';
 
-const IsOpen = () => {
+const IsOpen = ({openTime,closeTime}: {openTime: number, closeTime: number}) => {
     const [open, setOpen] = useState<string>('...');
 
 
     useEffect(() => {
-        const time = new Date().getHours();
-        if (time < 20 && time > 11) {
+        const timeNow = new Date().getHours();
+        if (timeNow < closeTime && timeNow > openTime) {
             setOpen('Открыто')
         } else {
             setOpen('Закрыто')
@@ -16,8 +16,8 @@ const IsOpen = () => {
     return (
         <div>
             <span
-                style={{ color: open == 'Открыто' ? 'green' : 'red', fontWeight: 700, fontSize: '10px' }}>
-                <b style={{ color: 'white' }}>сейчась </b>
+                style={{ color: open == 'Открыто' ? 'green' : 'red', fontWeight: 700, fontSize: '8px' }}>
+                <b style={{ color: 'white' }}> </b>
                 {open}
             </span>
         </div>
