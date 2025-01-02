a42f6f1c9a7c9e40c6a20bb04a3923e925720f24
Arman <aaproyan@bk.ru>
2025-01-02 16:09:38 +0400

    video add

--- app/components/VideoPlayer/VideoPlayer.tsx
+++ app/components/VideoPlayer/VideoPlayer.tsx
@@ -4,25 +4,25 @@ import styles from './style.module.scss';
 import dynamic from 'next/dynamic';
 
 const ReactPlayer = dynamic(() => import('react-player'), {ssr: false});
-
 const VideoPlayer: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-        <div className={styles.title}>
-            <span>Где мы находимся</span>
-        </div>
-    <div className={styles.playerWrapper}>
-    <iframe
-        className={styles.player}
-        src="https://www.youtube.com/embed/4cYwwam1QjA?autoplay=1&modestbranding=1&showinfo=0&rel=0"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </div>
+      <div className={styles.title}>
+        <span>Где мы находимся</span>
+      </div>
+      <div className={styles.playerWrapper}>
+        <ReactPlayer
+          className={styles.player}
+          url="https://www.youtube.com/watch?v=4cYwwam1QjA"
+          controls
+          playing
+          width="100%"
+          height="100%"
+        />
+      </div>
     </div>
   );
 };
+
 
 export default VideoPlayer;
